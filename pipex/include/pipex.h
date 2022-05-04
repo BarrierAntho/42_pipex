2cebb76d914ec101febb846b6fa1aae70f39e12c
Anthony BARRIER <abarrier@e2r7p12.clusters.42paris.fr>
2022-05-04 16:28:37 +0200

    UPDT - test execve, perror, strerror functions
    BODY:
    Test authorized functions
    
    TODO:
    N/A

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,19 +6,20 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:32:35 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/03 15:52:58 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/04 11:48:22 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
+# include <errno.h>
 # include <fcntl.h>
 # include <limits.h>
-
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 # include "ft_libft.h"
 
 int	ppx_parse(int argc, char **argv);
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:31:40 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/03 15:52:26 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/04 13:07:53 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv, char **envp)
 	i = 1;
 	(void)envp;
 
-	if (argc != 5)
+	if (argc != 4)
 		return (ft_error("main", "argc", 0, ERR_ARG));
 	else
 	{
@@ -29,6 +29,13 @@ int	main(int argc, char **argv, char **envp)
 			i++;
 		}
 	}
-	ppx_parse(argc, argv);
+	//ppx_parse(argc, argv);
+	int	ret;
+	ret = execve(argv[1], &argv[1], envp);
+	ft_dprintf(1, "ret = %d\n", ret);
+
+	ft_shell_msg("pipex", argv[1]);
+	if (ret != 0)
+		return (1);
 	return (0);
 }

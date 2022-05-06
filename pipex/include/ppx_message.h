82315526aa32146e1418877f25126b69a7852ef3
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-05-06 12:38:47 +0200

    UPDT - execve cat OK
    BODY:
        Test to get file, open it, dup2 to strin and execve cat command OK
    
    TODO:
    N/A

--- pipex/include/ppx_message.h
+++ pipex/include/ppx_message.h
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ppx_parse.c                                        :+:      :+:    :+:   */
+/*   ppx_message.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/03 14:47:35 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/03 16:08:02 by abarrier         ###   ########.fr       */
+/*   Created: 2022/05/06 09:52:29 by abarrier          #+#    #+#             */
+/*   Updated: 2022/05/06 09:59:20 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "pipex.h"
+#ifndef PPX_MESSAGE_H
+# define PPX_MESSAGE_H
 
-int	ppx_parse(int argc, char **argv)
-{
-	int	err;
+# include "pipex.h"
 
-	(void)argc;
-	(void)argv;
+# define ERR_PIPE "Pipe issue"
+# define ERR_FORK "Fork issue"
 
-	// TODO:
-	// for 2nd and last arguments (files)
-	// 	check access
-	// for 3rd to last last argument (commands)
-	// 	split char
-	// 	check access
-	
-	int ret = access("/../", F_OK);
-	ft_dprintf(1, "ret = %d\n", ret);
-	
-	err = 0;
-	return (err);
-}
+#endif
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:31:40 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/05 11:53:33 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/06 12:21:18 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	pid_t pid;
-	int	i;
+	pid_t	pid;
+	int		i;
+	int		npipe;
+	int		pfd[2];
 
 	i = 1;
 	(void)envp;
@@ -30,21 +32,47 @@ int	main(int argc, char **argv, char **envp)
 			i++;
 		}
 	}
-	int	pfd[2];
-	pipe(pfd);
-	ft_dprintf(1, "pfd[0] = %d\npfd[1] = %d\n", pfd[0], pfd[1]);
 
+	int ret = open(argv[1], O_RDONLY);
+	if (ret == -1)
+		return (ft_error("main", "open", 0, "ERROR"));
+	else
+		ft_dprintf(1, "ret = %d\n", ret);
+	dup2(ret, 0);
+	execve(argv[2], &argv[2], NULL);
+
+	npipe = argc - NOT_COMMAND - 1;
+
+	if(pipe(pfd) == -1)
+		return (ft_error("main", "pipe", 0, ERR_PIPE));
+	ft_dprintf(1, "pfd[0] = %d\n", pfd[0]);
+	ft_dprintf(1, "pfd[1] = %d\n", pfd[1]);
+
+	char buf;
+	while (read(pfd[1], &buf, 1) > 0)
+		write(1, &buf, 1);
+	ft_dprintf(1, "======================\n");
 	pid = fork();
+	if (pid == -1)
+		return (ft_error("main", "fork", 0, ERR_FORK));
 	if (pid == 0)
 	{
-		wait(NULL);
-		ft_dprintf(1, "This is parent\n");
+		close(pfd[1]);
+		dup2(pfd[0], 0);
+		execve(argv[2], &argv[2], NULL);
+		close(pfd[0]);
+		ft_dprintf(1, "END CHILD\n");
 	}
 	else
 	{
-		ft_dprintf(1, "This is child\n");
+		close(pfd[0]);
+		int ret_dup2 = dup2(ret, pfd[1]);
+		if (ret_dup2 == -1)
+			return (ft_error("main", "dup2", 0, "ERROR"));
+		close(pfd[1]);
+		wait(NULL);
+		ft_dprintf(1, "END PARENT\n");
 	}
-
 /*
 	int	ret;
 	ret = execve(argv[1], &argv[1], envp);

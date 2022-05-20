5fac950e81b67db4be7a36d591b525cd16bda34f
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-05-20 16:49:14 +0200

    UPDT - add populate pfd and close pfd
    BODY:
    Add populate pfd
    
    Add close pfd
    
    TODO:
    N/A
    
    modified:   srcs/pipex.c

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:31:40 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/19 18:28:47 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/20 15:57:46 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	main(int argc, char **argv, char **envp)
 {
 	t_list	**list;
 
-	if (argc != 5)
-		return (ft_error("main", "argc", 0, ERR_ARG));
+//	if (argc != 5)
+//		return (ft_error("main", "argc", 0, ERR_ARG));
 	list = ft_lst_init();
 	if (!list)
 		return (1);
@@ -26,8 +26,9 @@ int	main(int argc, char **argv, char **envp)
 		ft_lst_free(list, &ppx_cmd_free);
 		return (2);
 	}
-	ppx_pipex_run(argc, argv, list);
-	//ft_lst_show(list, &ppx_cmd_show);
+	ppx_lst_fd(argc, argv, list);
+	ppx_pipex_run(argc, list);
+	ft_lst_func_lst(list, &ppx_cmd_close_fd);
 	ft_lst_free(list, &ppx_cmd_free);
 	return (0);
 }

4903d55618e3e7aabba8ce9e724790bb79b121e5
BarrierAntho <barrier.antho@gmail.com>
2022-05-23 18:43:21 +0200

    UPDT - add new message forcommand not found
    modifié :         include/ppx_message.h

--- pipex/include/ppx_message.h
+++ pipex/include/ppx_message.h
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 09:52:29 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/20 09:36:55 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/23 18:30:12 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 # define ERR_PFD "Impossible to create a file descriptor"
 # define ERR_PIPE "Pipe issue"
 # define ERR_FORK "Fork issue"
+# define ERR_CMD_FOUND "Command not found"
 
 #endif

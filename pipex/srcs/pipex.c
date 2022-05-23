e3403a689d91434d6b7b1137cb8b3b7db65b4014
BarrierAntho <barrier.antho@gmail.com>
2022-05-23 13:40:15 +0200

    FIX - change function type to handle execve return value
    BODY:
    Change function type to handle execve return value and set to 0 or more than 0 fir the main result
    
    TODO:
    N/A
    modifié :         srcs/pipex.c

--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:31:40 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/20 17:50:18 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/23 08:25:03 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 int	main(int argc, char **argv, char **envp)
 {
 	t_list	**list;
+	int		res;
 
-	if (argc != 5)
-		return (ft_error("main", "argc", 0, ERR_ARG));
+//	if (argc != 5)
+//		return (ft_error("main", "argc", 0, ERR_ARG));
 	list = ft_lst_init();
+	res = 0;
 	if (!list)
 		return (1);
 	if (ppx_lst_set(argc, argv, envp, list))
@@ -27,8 +29,8 @@ int	main(int argc, char **argv, char **envp)
 		return (2);
 	}
 	ppx_lst_fd(argc, argv, list);
-	ppx_pipex_run(argc, list);
+	res = ppx_pipex_run(argc, list);
 	ft_lst_func_lst(list, &ppx_cmd_close_fd);
 	ft_lst_free(list, &ppx_cmd_free);
-	return (0);
+	return (res);
 }

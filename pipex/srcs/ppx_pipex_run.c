da9fa0c8cece4ba230c15de7a3a8ade3c7719473
BarrierAntho <barrier.antho@gmail.com>
2022-05-23 13:38:18 +0200

    FIX - change function type to handle execve return value
    BODY:
    Change function type to handle execve return value and set to 0 or more than 0 fir the main result
    
    TODO:
    N/A
    modifié :         srcs/ppx_pipex_run.c
    modifié :         srcs/ppx_pipex_wait.c

--- pipex/srcs/ppx_pipex_run.c
+++ pipex/srcs/ppx_pipex_run.c
@@ -6,22 +6,24 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 12:27:45 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/20 16:06:21 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/23 08:37:12 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ppx_pipex_run(int argc, t_list **list)
+int	ppx_pipex_run(int argc, t_list **list)
 {
 	t_list	*obj;
 	pid_t	pid;
 	int		npipe;
 	int		i;
+	int		res;
 
 	obj = *list;
 	npipe = argc - NOT_COMMAND - 1;
 	i = 0;
+	res = 0;
 	while (i < npipe && obj)
 	{
 		pid = fork();
@@ -35,4 +37,6 @@ void	ppx_pipex_run(int argc, t_list **list)
 		obj = obj->next;
 	}
 	ft_lst_func_lst(list, &ppx_cmd_close_fd);
+	res = ppx_pipex_wait(npipe);
+	return (res);
 }
--- pipex/srcs/ppx_pipex_wait.c
+++ pipex/srcs/ppx_pipex_wait.c
@@ -6,20 +6,26 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 16:00:11 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/20 16:01:04 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/23 08:41:38 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	ppx_pipex_wait(int npipe)
+int	ppx_pipex_wait(int npipe)
 {
+	int	res;
+	int	status;
 	int	i;
 
+	res = 0;
 	i = 0;
 	while (i < npipe)
 	{
-		waitpid(-1, NULL, 0);
+		waitpid(-1, &status, 0);
+		if (status > 0)
+			res++;
 		i++;
 	}
+	return (res);
 }

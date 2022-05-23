79cf3ffdd384480aa4a626558ec3938d87c5b503
BarrierAntho <barrier.antho@gmail.com>
2022-05-23 18:48:37 +0200

    FIX - add access condition to get error command not found
    modifié :         srcs/ppx_pipex_cmd.c

--- pipex/srcs/ppx_pipex_cmd.c
+++ pipex/srcs/ppx_pipex_cmd.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 12:09:43 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/23 08:29:30 by antho            ###   ########.fr       */
+/*   Updated: 2022/05/23 18:06:17 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,10 @@ void	ppx_pipex_cmd(t_list **list, t_list *obj)
 	dup2(cmd->fd_w, STDOUT_FILENO);
 	ft_lst_func_lst(list, &ppx_cmd_close_fd);
 	execve(cmd->fullcmd[0], cmd->fullcmd, NULL);
-	ft_shell_msg(errno, cmd->fullcmd[0]);
+	if (cmd->access == -1)
+		ft_dprintf(2, "Error: %s: %s\n", ERR_CMD_FOUND, cmd->fullcmd[0]);
+	else
+		ft_shell_msg(errno, cmd->fullcmd[0]);
 	ft_lst_free(list, &ppx_cmd_free);
 	exit(EXIT_FAILURE);
 }

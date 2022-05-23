78e0bc52a158c584c71d2856e59221473f8c0d6a
BarrierAntho <barrier.antho@gmail.com>
2022-05-23 13:30:42 +0200

    UPDT - change argument
    BODY:
    Change argumnent by replacing char arg to t_cmd arg and handle access property.
    
    TODO:
    N/A
    modifié :         srcs/cmd/ppx_cmd_getvalidpath.c

--- pipex/srcs/cmd/ppx_cmd_getvalidpath.c
+++ pipex/srcs/cmd/ppx_cmd_getvalidpath.c
@@ -6,27 +6,34 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:01:09 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/18 11:53:34 by abarrier         ###   ########.fr       */
+/*   Updated: 2022/05/23 08:14:35 by antho            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-char	*ppx_cmd_getvalidpath(char *cmd, char **envp)
+char	*ppx_cmd_getvalidpath(t_cmd *cmd, char **envp)
 {
 	char	*path;
 
 	path = NULL;
-	if (access(cmd, F_OK & X_OK) == 0)
-		path = ft_strdup(cmd);
+	if (access(cmd->fullcmd[0], F_OK & X_OK) == 0)
+		path = ft_strdup(cmd->fullcmd[0]);
 	else
 	{
-		path = ppx_cmd_loop_envp_str(cmd, envp, ENV_PATH_NAME);
+		path = ppx_cmd_loop_envp_str(cmd->fullcmd[0], envp, ENV_PATH_NAME);
 		if (path)
 			return (path);
-		path = ppx_cmd_loop_envp(cmd, envp);
+		path = ppx_cmd_loop_envp(cmd->fullcmd[0], envp);
+		if (path)
+			return (path);
 	}
 	if (!path)
-		path = ft_strdup(cmd);
+	{
+		path = ft_strdup(cmd->fullcmd[0]);
+		cmd->access = -1;
+	}
+	else
+		cmd->access = 0;
 	return (path);
 }

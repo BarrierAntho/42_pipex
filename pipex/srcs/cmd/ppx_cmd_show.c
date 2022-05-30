d51f05981fcbb5f0d6ce0a9e52773d2eb1c3a090
Anthony BARRIER <abarrier@e2r9p10.clusters.42paris.fr>
2022-05-30 10:28:20 +0200

    FIX - change printf flag s to d
    BODY:
    Change printf flag s to d
    
    TODO:
    N/A
    
    modified:   srcs/cmd/ppx_cmd_show.c

--- pipex/srcs/cmd/ppx_cmd_show.c
+++ pipex/srcs/cmd/ppx_cmd_show.c
@@ -6,7 +6,7 @@
 /*   By: abarrier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/17 15:10:12 by abarrier          #+#    #+#             */
-/*   Updated: 2022/05/23 08:17:46 by antho            ###   ########.fr       */
+/*   Updated: 2022/05/30 10:02:56 by abarrier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ void	ppx_cmd_show(void *content)
 	if (cmd->fullpath)
 		ft_dprintf(1, "fullpath: %s\n", cmd->fullpath);
 	ft_dprintf(1, "pfd[0]: %d\tpfd[1]: %d\n", cmd->fd_r, cmd->fd_w);
-	ft_dprintf(1, "access: %s\n", cmd->access);
+	ft_dprintf(1, "access: %d\n", cmd->access);
 	ft_dprintf(1, "%s\n", SEP_P);
 }
